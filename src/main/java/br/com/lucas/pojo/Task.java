feb28e8b109e509dca01ce6c21c3cb01388bcfc6
LucasAci <lucasqueiroz.aci@gmail.com>
2022-03-19 23:08:08 -0300

    Initial commit quarkus

---
+++ src/main/java/br/com/lucas/pojo/Task.java
@@ -0,0 +1,15 @@
+package br.com.lucas.pojo;
+
+public class Task {
+	public String key;
+	public int value;
+
+	public Task (String key, int value) {
+		this.key = key;
+		this.value = value;
+	}
+
+	public Task () {
+	}
+
+}
